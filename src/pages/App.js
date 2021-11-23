c0d388899cf000d87d20fc93d6358c8d32a57463
MiLLeRRain <xxdezz@gmail.com>
2021-11-24 02:16:17 +1300

    Router up

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,13 @@
+import React, {Component} from "react";
 import styles from './App.module.scss';
 import {Button} from "antd";
 import { Layout, Menu, Breadcrumb } from 'antd';
-
-const { Header, Content, Footer } = Layout;
+import {Content, Footer, Header} from "antd/es/layout/layout";
+import {Link, Route, Routes} from "react-router-dom";
+import HomePage from "./HomePage"
+import ProductPage from "./ProductPage"
+import ProductDetail from "./ProductDetail"
+import ContactPage from "./ContactPage"
 
 
 function App() {
@@ -11,16 +16,25 @@ function App() {
         <header className={styles.header}>
             <div className={styles.box}>
                 <div className={styles.boxleft}>DHPM</div>
-                <div className={styles.boxright}>Bread Navi here</div>
+                <div className={styles.boxright}>
+                        <Link to="/">Home</Link>
+                        <Link to="/product">Product</Link>
+                        <Link to="/contact">Contact</Link>
+                </div>
             </div>
         </header>
-        <Content className={styles.content}>
-            content
-        </Content>
-        <Footer className={styles.footer}>
+        <content className={styles.content}>
+            <Routes>
+            <Route path="/" component={HomePage} />
+            <Route path="/product" component={ProductPage} />
+            <Route path="/product/:id" component={ProductDetail} />
+            <Route path="/contact" component={ContactPage} />
+            </Routes>
+        </content>
+        <footer className={styles.footer}>
             <p><strong>@2021 DHPM</strong></p>
             <p>By L.H.</p>
-        </Footer>
+        </footer>
     </div>
   );
 }
